b3bd19edd084547fb0e44310d5aa66489cf35c43
MehmetAydar01 <mehmet.aydar07@gmail.com>
2023-12-30 02:42:33 +0300

    Landing Home Page created and datas caching

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { useLoaderData } from 'react-router-dom';
+import CocktailList from '../component/CocktailList';
+import SearchForm from '../component/SearchForm';
+const cocktailSearchUrl =
+  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
+import { useQuery } from '@tanstack/react-query';
+
+const searchCocktailsQuery = (searchTerm) => {
+  return {
+    queryKey: ['search', searchTerm || 'all'],
+    queryFn: async () => {
+      const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+      return response.data.drinks;
+    },
+  };
+};
+
+export const Loader =
+  (queryClient) =>
+  async ({ request }) => {
+    const url = new URL(request.url);
+
+    const searchTerm = url.searchParams.get('search') || '';
+    await queryClient.ensureQueryData(searchCocktailsQuery(searchTerm));
+
+    return { searchTerm };
+  };
+
+const Landing = () => {
+  const { searchTerm } = useLoaderData();
+  const { data: drinks } = useQuery(searchCocktailsQuery(searchTerm));
+
+  return (
+    <>
+      <SearchForm searchTerm={searchTerm} />
+      <CocktailList drinks={drinks} />
+    </>
+  );
+};
+
+export default Landing;
